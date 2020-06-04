00c335b56c14b65fe70f9758888f1d1e60397e3b
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-06-04 16:18:16 +0300

    v1.0.1stable added SearchForm, LocalTime

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import SearchForm from './Search/SearchForm'
+import LocalTime from './Search/LocalTime'
 
 function App() {
     return (
         <div className='wrapper'>
             <div className='search-body'>
-                <div className='search-body__search-form'></div>
-                <div className='search-body__local-time'></div>
+                <div className='search-body__search-form'>
+                    <SearchForm></SearchForm>
+                </div>
+                <div className='search-body__local-time'>
+                    <LocalTime></LocalTime>
+                </div>
             </div>
             <div className='main-body'>
                 <div className='main-body__media'>
---
+++ src/Search/LocalTime.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LocalTime() {
+    return (
+        <div className='LocalTime'>
+            <span className='time'>time</span>
+        </div>
+    )
+}
