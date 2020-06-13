f7942895db221910a20e54c153ed47096bc67dfd
vernigora2k <vernigora2k@gmail.com>
2020-06-13 17:24:01 +0300

    v1.3.9temp

--- src/FovoriteCities/FavoriteCityList.js
+++ src/FovoriteCities/FavoriteCityList.js
@@ -3,21 +3,39 @@ import './favorite-city-list.scss'
 import FavoriteCity from './FavoriteCity'
 
 export default function FavoriteCityList({ citiesList }) {
-    
-    console.log(!![...citiesList].length)
-    if (!!![...citiesList].length) {
-        citiesList = ['']
+    console.log('I am a favoriteCity inner))')
+    console.log(!!citiesList.size)
+    if (!citiesList.size) {
+        //citiesList = ['']
         console.log('false')
     }
+
+    // function FavoriteCity() {
+    //     if (citiesList.size) {
+    //         [...citiesList].map((city,i) => {
+    //             return <FavoriteCity city={city} key={i}></FavoriteCity>
+    //         })
+    //     }
+    // }
+
+    let isCityListEmpty = !!!citiesList.size
+
     return (
         <div className='favorite-cities'>
             <div className='favorite-cities__title'>
                 <span>Added Locations:</span>
             </div>
             <div className='favorite-cities__list flex'>
-                {[...citiesList].map((city,i) => {
+                {/* {[...citiesList].map((city,i) => {
                     return <FavoriteCity city={city} key={i}></FavoriteCity>
-                })}
+                })} */}
+
+                {isCityListEmpty ? '' :
+                    ([...citiesList].map((city,i) => {
+                        return <FavoriteCity city={city} key={i}></FavoriteCity>
+                    }))
+                }
+                
             </div>
         </div>
     )
