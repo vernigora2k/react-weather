f2d853f3f1c890d357c12e8e9aa175f1df590397
vernigora2k <vernigora2k@gmail.com>
2020-06-16 16:05:31 +0300

    v1.5.7temp

--- src/WeatherNow/Weather.js
+++ src/WeatherNow/Weather.js
@@ -1,42 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './weather.scss'
+import Context from '../context'
 import { getWeather, getLocalTime } from '../js/controller'
 
-export function showWeather(city) {
-    getWeather(city)
-    .then(response => {
-        const data = response.data[0]
-        const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data
-        // mainScreenTemp.textContent = Math.round(temp)
-        // mainScreenWeatherDescription.textContent = description
-        // mainScreenWeatherIcon.src = `src/img/weather-icons/${icon}.png`
-        // mainScreenActivatedCity.textContent = searchFormInput.value
-        // windDir.textContent = wind_cdir_full 
-        // windSpeed.textContent = Math.round(wind_spd) + ' m/s'
-        // pressure.textContent = Math.round(pres) + ' mb'
-        // sunriseProp.textContent = sunrise
-        // sunsetProp.textContent = sunset
-        // radiation.textContent = Math.round(solar_rad) + ' W/m^2'
-        getLocalTime(timezone)
-            .then(response => {
-                //mainScreenTime.textContent = response.datetime.slice(11,16)
-                console.log(response.datetime.slice(11,16))
-            })
-            .catch('errorHandler')
-        //localStorage.setItem('lastWatchedCity', city)
-        })
-    // .then(() => {
-    //     if(checkfavoriteCityDublicate(mainScreenActivatedCity.textContent)) {
-    //         iconHeartImg.src = 'src/img/heart-black.svg'
-    //     } else {
-    //         iconHeartImg.src = 'src/img/heart-white.svg'
-    //     }
-    // })
-    .catch('errorHandler')
-}
-
+const { favoriteName } = useContext(Context)
+//showWeather(favoriteName)
+console.log(favoriteName)
 
 export default function Weather() {
+
+    function showWeather(city) {
+        getWeather(city)
+        .then(response => {
+            const data = response.data[0]
+            const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data
+            getLocalTime(timezone)
+                .then(response => {
+                    console.log(response.datetime.slice(11,16))
+                })
+                .catch('errorHandler')
+            })
+        .catch('errorHandler')
+    }
+
     return (
         <div className='weather'>
             <div className='weather__temp'>35deg</div>
