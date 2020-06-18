c56ec678e68225a9ca47a1a2d5f6e081fad33e6c
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-06-19 00:14:30 +0300

    v1.6.0temp

--- src/WeatherNow/Weather.js
+++ src/WeatherNow/Weather.js
@@ -7,25 +7,22 @@ import { getWeather, getLocalTime } from '../js/controller'
 
 export default function Weather() {
     const { searchFormValue, changeTime } = useContext(Context)
-    showWeather(searchFormValue)
-
-    function showWeather(city) {
-        getWeather(city)
-        .then(response => {
-            const data = response.data[0]
-            const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data
-            getLocalTime(timezone)
-                .then(response => {
-                    changeTime(response.datetime.slice(11,16))
-                })
-                .catch('errorHandler')
+   
+    getWeather(searchFormValue)
+    .then(response => {
+        const data = response.data[0]
+        const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data
+        getLocalTime(timezone)
+            .then(response => {
+                changeTime(response.datetime.slice(11,16))
             })
-        .catch('errorHandler')
-    }
+            .catch('errorHandler')
+        })
+    .catch('errorHandler')
 
     return (
         <div className='weather'>
-            <div className='weather__temp'>35deg</div>
+            <div className='weather__temp'>{temp}</div>
             <div className='weather__icon flex'>
                 <div className='weather__icon-description'>clouds</div>
                 <div className='weather__icon-img'>
