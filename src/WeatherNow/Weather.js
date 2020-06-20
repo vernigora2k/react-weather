0309a4cbb98ce5d7dc12e2e6513434a99760d138
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-06-20 23:48:56 +0300

    v1.6.4temp

--- src/WeatherNow/Weather.js
+++ src/WeatherNow/Weather.js
@@ -3,44 +3,27 @@ import './weather.scss'
 import Context from '../context'
 import { getWeather, getLocalTime } from '../js/controller'
 
-
-
 export default function Weather() {
     const { searchFormValue, changeTime } = useContext(Context)
-    const [state, setState] = useState()
-    
+    const [state, setState] = useState('')
+
     getWeather(searchFormValue)
     .then(response => {
         const data = response.data[0]
-        const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data
-        
-        //setState({temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad})
-        console.log(state)
-        
+        const {temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad} = data 
         getLocalTime(timezone)
             .then(response => {
                 changeTime(response.datetime.slice(11,16))
             })
             .catch('errorHandler')
-        console.log(temp)
-        return temp
     })
-    .then(
-        //res => {console.log(res)}
-        useEffect(temp => {
-            //function changeState() {
-                //setState({temp, weather: {description, icon}, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad})
-                //setState(res)
-                console.log(temp)
-                
-            //}
-        })
-    )
     .catch('errorHandler')
+
+    useEffect(() => {setState(temp, description, icon, timezone, wind_cdir_full, wind_spd, pres, sunrise, sunset, solar_rad)})
 
     return (
         <div className='weather'>
-            <div className='weather__temp'></div>
+            <div className='weather__temp'>{state.temp}</div>
             <div className='weather__icon flex'>
                 <div className='weather__icon-description'>clouds</div>
                 <div className='weather__icon-img'>
--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const URLweather = "https://api.weatherbit.io/v2.0/current?units=M&key=5421c72129ae4940b8ee9d8dbf36649c&city=";
+export const URLweather = "https://api.weatherbit.io/v2.0/current?units=M&key=cf37e5fa9bdb4e0b965f9620b3c223f4&city=";
 export const URLtime = "https://worldtimeapi.org/api/timezone/";
 export const URLforecast = "https://api.weatherbit.io/v2.0/forecast/daily?units=M&key=b7c124d4d1cf4643949fa516b7fe633e&city=";
 
