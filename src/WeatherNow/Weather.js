980b9d9a0b137231a0125ff39b949174e693c6f0
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-06-24 21:48:13 +0300

    v1.8.2stable

--- src/WeatherNow/Weather.js
+++ src/WeatherNow/Weather.js
@@ -28,7 +28,7 @@ export default function Weather() {
         iconImg = state.weather.icon
     }
     
-    if(mainMenuActiveBtn == 'now') {
+    if(mainMenuActiveBtn === 'now') {
         return (
             <div className='weather'>
                 <div className='weather__temp'>{Math.round(state.temp)}<sup>0</sup></div>
@@ -48,7 +48,7 @@ export default function Weather() {
         )
     }
 
-    if(mainMenuActiveBtn == 'details') {
+    if(mainMenuActiveBtn === 'details') {
         return (
             <div className='weather flex' style={{justifyContent: 'space-between'}}>
                 <div className='weather__temp'>{Math.round(state.temp)}<sup>0</sup></div>
@@ -66,49 +66,46 @@ export default function Weather() {
                 </div>
 
                 <div className="media__details">
-                            <table className="table-details" width='300px' height='200px'>
-                                <tr>
-                                    <td className="cell-prop-wind">
-                                        <div className="flex">
-                                            <img src={require(`./Details/img/wind.svg`)} alt="wind" width="50px" style={{paddingTop: 3 +'px'}} ></img>
-                                            <span className="wind-dir">{state.wind_cdir_full}</span>
-                                            <span className="wind-speed">{Math.round(state.wind_spd)} m/s</span>
-                                        </div>
-                                    </td>
-                                    <td className="cell-prop-pres">
-                                        <div className="flex">
-                                            <img src={require(`./Details/img/pressure.svg`)} alt="pressure" width="40px"></img>
-                                            <span className="pressure">{Math.round(state.pres)} mb</span>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className="cell-prop-sunrize">
-                                        <div className="flex">
-                                            <img src={require(`./Details/img/sun.svg`)} alt="sunrize" width="20px"></img>
-                                            <span className="sunrize">{state.sunrise}</span>
-                                            <img src={require(`./Details/img/moon.svg`)} alt="sunset" width="20px"></img>
-                                            <span className="sunset">{state.sunset}</span>
-                                        </div>
-                                    </td>
-                                    <td className="cell-prop-solarrad">
-                                        <div className="flex">
-                                            <div>
-                                                <img src={require(`./Details/img/sun.svg`)} alt="sun" width="25px"></img>
-                                                <img src={require(`./Details/img/radiation.svg`)} alt="radiation" width="25px"></img>
-                                            </div>
-                                            <div>
-                                                <span style={{fontSize: 19 +'px'}}>solar-radiation</span>
-                                            </div>
-                                                <span className="radiation">{Math.round(state.solar_rad)} W/m^2</span>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-
-
-
+                    <table className="table-details" width='300px' height='200px'>
+                        <tr>
+                            <td className="cell-prop-wind">
+                                <div className="flex">
+                                    <img src={require(`./Details/img/wind.svg`)} alt="wind" width="50px" style={{paddingTop: 3 +'px'}} ></img>
+                                    <span className="wind-dir">{state.wind_cdir_full}</span>
+                                    <span className="wind-speed">{Math.round(state.wind_spd)} m/s</span>
+                                </div>
+                            </td>
+                            <td className="cell-prop-pres">
+                                <div className="flex">
+                                    <img src={require(`./Details/img/pressure.svg`)} alt="pressure" width="40px"></img>
+                                    <span className="pressure">{Math.round(state.pres)} mb</span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="cell-prop-sunrize">
+                                <div className="flex">
+                                    <img src={require(`./Details/img/sun.svg`)} alt="sunrize" width="20px"></img>
+                                    <span className="sunrize">{state.sunrise}</span>
+                                    <img src={require(`./Details/img/moon.svg`)} alt="sunset" width="20px"></img>
+                                    <span className="sunset">{state.sunset}</span>
+                                </div>
+                            </td>
+                            <td className="cell-prop-solarrad">
+                                <div className="flex">
+                                    <div>
+                                        <img src={require(`./Details/img/sun.svg`)} alt="sun" width="25px"></img>
+                                        <img src={require(`./Details/img/radiation.svg`)} alt="radiation" width="25px"></img>
+                                    </div>
+                                    <div>
+                                        <span style={{fontSize: 19 +'px'}}>solar-radiation</span>
+                                    </div>
+                                        <span className="radiation">{Math.round(state.solar_rad)} W/m^2</span>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>            
         )
     }
