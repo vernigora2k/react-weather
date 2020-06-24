5af9487312495fa7806beb9fe5f18404e3d4e373
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-06-24 21:40:34 +0300

    v1.8.1stable

--- src/WeatherNow/Weather.js
+++ src/WeatherNow/Weather.js
@@ -70,37 +70,37 @@ export default function Weather() {
                                 <tr>
                                     <td className="cell-prop-wind">
                                         <div className="flex">
-                                            <img src="./src/img/wind.svg" alt="wind" width="50px" style={{paddingTop: 3 +'px'}} ></img>
-                                            <span className="wind-dir">north</span>
-                                            <span className="wind-speed"></span>
+                                            <img src={require(`./Details/img/wind.svg`)} alt="wind" width="50px" style={{paddingTop: 3 +'px'}} ></img>
+                                            <span className="wind-dir">{state.wind_cdir_full}</span>
+                                            <span className="wind-speed">{Math.round(state.wind_spd)} m/s</span>
                                         </div>
                                     </td>
                                     <td className="cell-prop-pres">
                                         <div className="flex">
-                                            <img src="./src/img/pressure.svg" alt="pressure" width="40px"></img>
-                                            <span className="pressure">780 mb</span>
+                                            <img src={require(`./Details/img/pressure.svg`)} alt="pressure" width="40px"></img>
+                                            <span className="pressure">{Math.round(state.pres)} mb</span>
                                         </div>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td className="cell-prop-sunrize">
                                         <div className="flex">
-                                            <img src="./src/img/sun.svg" alt="sunrize" width="20px"></img>
-                                            <span className="sunrize">sunrize</span>
-                                            <img src="./src/img/moon.svg" alt="sunset" width="20px"></img>
-                                            <span className="sunset">sunset</span>
+                                            <img src={require(`./Details/img/sun.svg`)} alt="sunrize" width="20px"></img>
+                                            <span className="sunrize">{state.sunrise}</span>
+                                            <img src={require(`./Details/img/moon.svg`)} alt="sunset" width="20px"></img>
+                                            <span className="sunset">{state.sunset}</span>
                                         </div>
                                     </td>
                                     <td className="cell-prop-solarrad">
                                         <div className="flex">
                                             <div>
-                                                <img src="./src/img/sun.svg" alt="sun" width="25px"></img>
-                                                <img src="./src/img/radiation.svg" alt="radiation" width="25px"></img>
+                                                <img src={require(`./Details/img/sun.svg`)} alt="sun" width="25px"></img>
+                                                <img src={require(`./Details/img/radiation.svg`)} alt="radiation" width="25px"></img>
                                             </div>
                                             <div>
                                                 <span style={{fontSize: 19 +'px'}}>solar-radiation</span>
                                             </div>
-                                                <span className="radiation"></span>
+                                                <span className="radiation">{Math.round(state.solar_rad)} W/m^2</span>
                                         </div>
                                     </td>
                                 </tr>
